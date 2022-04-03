262e79ae35d6f7177ebfaddbe0253b2573b6814a
ChunChun123-boy <1390771302@qq.com>
2022-04-04 00:38:21 +0800

    use for loop

--- inf371_spring2022-ChunChun123-boy-main/Digital Game Design and Development/1forList.py
+++ inf371_spring2022-ChunChun123-boy-main/Digital Game Design and Development/1forList.py
@@ -1,4 +1,4 @@
-#Python3 code to iterate over a list
+# Python3 code to iterate over a list
 list=[1,3,5,7,9]
 
 length=len(list)
---
+++ inf371_spring2022-ChunChun123-boy-main/Digital Game Design and Development/2forRange.py
@@ -0,0 +1,3 @@
+import random
+for i in range(5):
+    print(i, end=", ") # prints: 0, 1, 2, 3, 4,
